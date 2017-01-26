8c62489337b3b9d8ee815996bc6f7749820d50a7
chesno_a <bilal.chesnoy@epitech.eu>
2017-01-26 21:04:10 +0100

    Added script that says if RTM ticket has been used (more info with -h option

---
+++ is_used_ticket.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+ISF=$'\n'
+if [ $# \> 0 ];then
+    while [ $# \> 0 ];do
+	if [ "$1" == "-h" ] || [ "$1" == "--help" ];then
+	    echo -e "./is_used_ticket.sh [ticket.dump] [ticket2.dump] [ticket3.dump] ...\n Script will check all dumps in current directory if no file specified"
+	fi
+	ALL=`hexdump $1 | awk -F " " '{print $NF}'`
+	
+	for line in $ALL;do
+	    if [ "$line" == "0000" ];then
+		echo -e "$1: \033[32mNot used\033[0m"
+	    elif [ "$line" == "8000" ];then
+		echo -e "$1: \033[33mUsed one time\033[0m"
+	    else
+		echo -e "$1: \033[31mUsed more than one time ($line)\033[0m"
+	    fi
+	    break
+	done
+	shift
+    done
+else
+    ALL_FILES=`ls -l *.dump | awk -F " " '{print $NF}'`
+
+    for file in $ALL_FILES;do
+	ALL=`hexdump $file | awk -F " " '{print $NF}'`
+
+	for line in $ALL;do
+	    if [ "$line" == "0000" ];then
+		echo -e "$file: \033[32mNot used\033[0m"
+	    elif [ "$line" == "8000" ];then
+		echo -e "$file: \033[33mUsed one time\033[0m"
+	    else
+		echo -e "$file: \033[31mUsed more than one time ($line)\033[0m"
+	    fi
+	    break
+	done
+    done
+fi
